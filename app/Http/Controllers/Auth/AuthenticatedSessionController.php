9d8a83b8f23e60ff66cf237936f1da17f8f4e6d1
220101067 <rezaadityathohir30@gmail.com>
2025-06-24 13:49:21 +0900

    Menambahkan role admin dan user + middleware

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -29,7 +29,14 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(RouteServiceProvider::HOME);
+
+        $user = Auth::user();
+
+        if ($user->role === 'admin') {
+            return redirect()->intended('/dashboard');
+        }
+
+        return redirect()->intended('/');
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -22,33 +22,24 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth', 'role:admin'])->name('dashboard');
 
 // Semua route ini hanya bisa diakses jika user sudah login
-Route::middleware(['auth'])->group(function () {
+Route::middleware(['auth', 'role:admin'])->group(function () {
 
     // ==================
     // ROUTE CRUD BUKU
     // ==================
 
-    // Menampilkan daftar buku
     Route::get('/buku', [BukuController::class, 'index'])->name('buku.index');
-
-    // Form tambah buku
     Route::get('/buku/create', [BukuController::class, 'create'])->name('buku.create');
-
-    // Simpan buku baru
     Route::post('/buku', [BukuController::class, 'store'])->name('buku.store');
-
-    // Form edit buku
     Route::get('/buku/{id}/edit', [BukuController::class, 'edit'])->name('buku.edit');
-
-    // Update data buku
     Route::put('/buku/{id}', [BukuController::class, 'update'])->name('buku.update');
-
-    // Hapus data buku
     Route::delete('/buku/{id}', [BukuController::class, 'destroy'])->name('buku.destroy');
+
 });
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
